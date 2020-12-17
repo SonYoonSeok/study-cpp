2bcad1aaf556ebe0b3b6a2970c0949094bb9845b
dbstjr5517 <dbstjr5517@naver.com>
2020-12-17 22:48:15 +0900

    12/17 10:47 1장 프로젝트 완료 p125

--- LearnCPP/Main.cpp
+++ LearnCPP/Main.cpp
@@ -38,6 +38,7 @@ int main()
 			break;
 		case 5:
 			employeeDB.displayCurrent();
+			break;
 		default:
 			cerr << "Unknown command." << endl;
 			break;
@@ -58,4 +59,80 @@ int displayMenu()
 	cout << "4) List all employees" << endl;
 	cout << "5) List current employees" << endl;
 	cout << "0) Quit" << endl;
+	cout << endl;
+	cout << "---> ";
+	cin >> selection;
+	return selection;
+}
+
+void doHire(Database& db)
+{
+	string firstName;
+	string lastName;
+
+	cout << "First Name? ";
+	cin >> firstName;
+	cout << "Last Name? ";
+	cin >> lastName;
+
+	db.addEmployee(firstName, lastName);
+}
+
+void doFire(Database& db)
+{
+	int employeeNumber;
+
+	cout << "Employee Number? ";
+	cin >> employeeNumber;
+
+	try
+	{
+		Employee& emp = db.getEmployee(employeeNumber);
+		emp.fire();
+		cout << "Employee " << employeeNumber << " terminated." << endl;
+	}
+	catch (const std::logic_error& exception) {
+		cerr << "Unable to terminate employee: " << exception.what() << endl;
+	}
+}
+
+void doPromote(Database& db)
+{
+	int employeeNumber;
+	int raiseAmount;
+
+	cout << "Employee Number? ";
+	cin >> employeeNumber;
+
+	cout << "How much of a raise? ";
+	cin >> raiseAmount;
+
+	try {
+		Employee& emp = db.getEmployee(employeeNumber);
+		emp.promote(raiseAmount);
+	}
+	catch (const std::logic_error& exception) {
+		cerr << "Unable to promote employee: " << exception.what() << endl;
+	}
+}
+
+void doDemote(Database& db)
+{
+	int employeeNumber;
+	int decreaseAmount;
+
+	cout << "Employee Number? ";
+	cin >> employeeNumber;
+
+	cout << "How much of a decrease? ";
+	cin >> decreaseAmount;
+
+	try {
+		Employee& emp = db.getEmployee(employeeNumber);
+		emp.demote(decreaseAmount);
+
+	}
+	catch (const std::logic_error& exception) {
+		cerr << "Unable to promote employee: " << exception.what() << endl;
+	}
 }
