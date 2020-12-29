46b1658fd5a7c1b42efd9559b10231291065075c
dbstjr5517 <dbstjr5517@naver.com>
2020-12-29 09:19:30 +0900

    12/29 9:19 삽입정렬

---
+++ LearnCPP/삽입정렬.cpp
@@ -0,0 +1,35 @@
+#pragma warning(disable: 4996)
+#include <stdio.h>
+
+int array[1001];
+
+int main()
+{
+	int number, i, j, min, index, tmp;
+	scanf("%d", &number);
+	for (i=0; i<number; i++)
+	{
+		scanf("%d", &array[i]);
+	}
+
+	for (i = 0; i < number; i++)
+	{
+		min = 1001;
+		for (j=i; j<number; j++)
+		{
+			if (min > array[j])
+			{
+				min = array[j];
+				index = j;
+			}
+		}
+		tmp = array[i];
+		array[i] = array[index];
+		array[index] = tmp;
+	}
+
+	for (i = 0; i < number; i++)
+	{
+		printf("%d ", array[i]);
+	}
+}
