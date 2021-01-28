3c0c3669458dc66033cb4dcfa8700f9699696f83
Son Yoon Seok <dbstjr5517@naver.com>
2021-01-28 12:04:56 +0900

    1/28 12:04
    p.73

---
+++ LearnCPP/constRe.cpp
@@ -0,0 +1,13 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int val1 = 10, val2 = 20, val3 = 30;
+//	const int num = 20; // 변수 num을 상수화
+//	const int* ptr1 = &val1; // 포인터 ptr1을 이용해서 val1의 값을 변경할 수 없음
+//	int* const ptr2 = &val2; // 포인터 ptr2가 상수화 됨
+//	const int* const ptr3 = &val3; // 포인터 ptr3가 상수화 되었으며, ptr3을 이용해서 val3의 값을 변경할 수 없음
+//
+//	cout << num << " " << ptr1 << " " << ptr2 << " " << ptr3 << " ";
+//}
